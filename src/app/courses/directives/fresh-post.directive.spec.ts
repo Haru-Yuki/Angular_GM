60b801439e39e58c759885f4ebbde847eb10c7a7
Haru-Yuki <dimakred@gmail.com>
2021-04-29 17:07:24 +0300

    fix: test fix

--- src/app/courses/directives/fresh-post.directive.spec.ts
+++ src/app/courses/directives/fresh-post.directive.spec.ts
@@ -1,8 +1,9 @@
 import { FreshPostDirective } from './fresh-post.directive';
+import { ElementRef } from '@angular/core';
 
 describe('FreshPostDirective', () => {
   it('should create an instance', () => {
-    const directive = new FreshPostDirective();
+    const directive = new FreshPostDirective(ElementRef as any);
     expect(directive).toBeTruthy();
   });
 });
