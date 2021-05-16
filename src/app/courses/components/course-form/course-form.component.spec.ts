2349c54c37bd8ecf1c965b778c80db90fc77dccd
Haru-Yuki <dimakred@gmail.com>
2021-05-16 17:17:15 +0300

    fix: fixed test for course form

--- src/app/courses/components/course-form/course-form.component.spec.ts
+++ src/app/courses/components/course-form/course-form.component.spec.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { CourseFormComponent } from './course-form.component';
 
@@ -8,7 +10,8 @@ describe('CourseFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CourseFormComponent ]
+      declarations: [ CourseFormComponent ],
+      providers: [ FormBuilder, DatePipe ]
     })
     .compileComponents();
   });
